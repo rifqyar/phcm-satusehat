c2b3e31f66ec1708ca0171699bc49674a2d0147e
rifqyar <rifqyaditya55@gmail.com>
2025-10-14 11:22:14 +0700

    breadcrumb encounter

--- resources/views/pages/satusehat/encounter/index.blade.php
+++ resources/views/pages/satusehat/encounter/index.blade.php
@@ -36,8 +36,8 @@
         <div class="col-md-5 col-8 align-self-center">
             <h3 class="text-themecolor">Dashboard</h3>
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="javascript:void(0)">Encounter</a></li>
-                <li class="breadcrumb-item active">Dashboard</li>
+                <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+                <li class="breadcrumb-item active">Encounter</li>
             </ol>
         </div>
         <div class="col-md-7 col-4 align-self-center">
