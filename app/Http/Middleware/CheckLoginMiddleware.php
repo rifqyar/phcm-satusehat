8def5c5574ebb4a50a7adea1fa700305684d00b2
ersarb <ersa.rb26@gmail.com>
2025-12-14 22:39:45 +0700

    skip middleware buat kirim biar gak error 302

--- app/Http/Middleware/CheckLoginMiddleware.php
+++ app/Http/Middleware/CheckLoginMiddleware.php
@@ -15,22 +15,39 @@ class CheckLoginMiddleware
      * @return mixed
      */
     public function handle($request, Closure $next)
+
+
     {
-        if ($_SERVER['REMOTE_ADDR'] == '::1') {
-            if (Session::has('is_logged_in')) {
-                config(['session.lifetime' => 1440]);
-                return $next($request);
-            } else {
-                Session::invalidate();
-                Session::regenerateToken();
-                return redirect('login');
-            }
-        } else {
-            if (ci_session('sdh_masuk_simrs') !== true) {
-                return redirect('http://10.1.19.22/login');
-            }
+
+
+        if (Session::has('is_logged_in')) {
+
+
+            config(['session.lifetime' => 1440]);
+
 
             return $next($request);
+
+
+
+
+
+
+
+        } else {
+
+
+            Session::invalidate();
+
+
+            Session::regenerateToken();
+
+
+            return redirect('login');
+
+
+
         }
+
     }
 }
