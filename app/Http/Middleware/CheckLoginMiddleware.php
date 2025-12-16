99eeb448b73a2037b2c7a6f907b8c9477024959d
rifqyar <rifqyaditya55@gmail.com>
2025-12-16 13:50:35 +0700

    fix middleware

--- app/Http/Middleware/CheckLoginMiddleware.php
+++ app/Http/Middleware/CheckLoginMiddleware.php
@@ -16,32 +16,43 @@ class CheckLoginMiddleware
      */
     public function handle($request, Closure $next)
     {
-        // 1. Kalau sudah login di Laravel → lanjut
-        if (session()->has('ci_synced')) {
-            return $next($request);
-        }
-
-        // 2. Jangan cek CI untuk request asset
-        if ($request->is('css/*', 'js/*', 'images/*', 'favicon.ico')) {
-            return $next($request);
-        }
-
-        // 3. Coba sinkron dari CI (1x saja)
-        try {
-            $loggedIn = ci_session('sdh_masuk_simrs');
-
-            if ($loggedIn === true) {
-                session()->put('ci_synced', true);
-                session()->save();
+        if ($_SERVER['REMOTE_ADDR'] == '::1' || $_SERVER['REMOTE_ADDR'] == '103.234.195.158') {
+            if (Session::has('is_logged_in')) {
+                config(['session.lifetime' => 1440]);
+                return $next($request);
+            } else {
+                Session::invalidate();
+                Session::regenerateToken();
+                return redirect('login');
+            }
+        } else {
+            // 1. Kalau sudah login di Laravel → lanjut
+            if (session()->has('ci_synced')) {
                 return $next($request);
             }
-        } catch (\Throwable $e) {
-            logger()->warning('CI auth bridge failed', [
-                'error' => $e->getMessage()
-            ]);
-        }
 
-        // 4. GAGAL → redirect
-        return redirect('https://sim.phcm.co.id/simrs');
+            // 2. Jangan cek CI untuk request asset
+            if ($request->is('css/*', 'js/*', 'images/*', 'favicon.ico')) {
+                return $next($request);
+            }
+
+            // 3. Coba sinkron dari CI (1x saja)
+            try {
+                $loggedIn = ci_session('sdh_masuk_simrs');
+
+                if ($loggedIn === true) {
+                    session()->put('ci_synced', true);
+                    session()->save();
+                    return $next($request);
+                }
+            } catch (\Throwable $e) {
+                logger()->warning('CI auth bridge failed', [
+                    'error' => $e->getMessage()
+                ]);
+            }
+
+            // 4. GAGAL → redirect
+            return redirect('https://sim.phcm.co.id/simrs');
+        }
     }
 }
