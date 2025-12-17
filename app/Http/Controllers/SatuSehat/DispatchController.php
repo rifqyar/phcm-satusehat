11af8ee8e3f36d72592360e1c35d5ab14dbaf29d
rifqyar <rifqyaditya55@gmail.com>
2025-12-17 10:50:31 +0700

    tet

--- app/Http/Controllers/SatuSehat/DispatchController.php
+++ app/Http/Controllers/SatuSehat/DispatchController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\SatuSehat;
 
 use App\Http\Controllers\Controller;
+use App\Http\Traits\LogTraits;
 use App\Jobs\DispatchCIRequest;
 use Illuminate\Http\Request;
 
 class DispatchController extends Controller
 {
+    use LogTraits;
     public function dispatchController(Request $request)
     {
+        $this->logInfo('dispatchci', 'Receive param', [
+            'payload' => $request,
+        ]);
+
         $url = $request->url;
         DispatchCIRequest::dispatch($request, $url);
     }
