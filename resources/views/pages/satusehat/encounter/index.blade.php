95f5c4b11d32c63a5fbb8b772ae3952403bfcfbf
rifqyar <rifqyaditya55@gmail.com>
2026-01-24 11:37:14 +0700

    perbaikan encounter

--- resources/views/pages/satusehat/encounter/index.blade.php
+++ resources/views/pages/satusehat/encounter/index.blade.php
@@ -311,11 +311,11 @@
                         data.tgl_akhir = $('input[name="tgl_akhir"]').val();
                     },
                     dataSrc: function(json) {
-                        $('#total_all').text(json.total_semua)
-                        $('#total_rj').text(json.rjAll)
-                        $('#total_ri').text(json.ri)
-                        $('#total_integrasi').text(json.total_sudah_integrasi)
-                        $('#total_belum_integrasi').text(json.total_belum_integrasi)
+                        $('#total_all').text(json.summary.total_semua)
+                        $('#total_rj').text(json.summary.rjAll)
+                        $('#total_ri').text(json.summary.ri)
+                        $('#total_integrasi').text(json.summary.total_sudah_integrasi)
+                        $('#total_belum_integrasi').text(json.summary.total_belum_integrasi)
                         return json.data
                     }
                 },
